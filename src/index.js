f73264300f7f43e6cd0ac28f1f51148ae77c539c
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-03-08 10:15:02 +0100

    Add form to event listener

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".main-section form");
+const input = document.querySelector(".main-section input");
+const msg = document.querySelector(".main-section .msg");
+const list = document.querySelector(".ajax-section .cities");
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const inputVal = input.value;
+});
