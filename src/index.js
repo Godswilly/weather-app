78b3c07a301546e826bf8152d8a2d1eca944a760
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-03-11 16:20:59 +0100

    Fetch data from the API

--- src/index.js
+++ src/index.js
@@ -15,3 +15,42 @@ const myKey = 'c2bfcc2580b3b62b338b0c119345b4fc';
 const kelvinToCelsius = (temp) => `${(temp - 273.15).toFixed(0)}°C`;
 
 const kelvinToFarenheit = (temp) => `${((temp - 273.15) * (9 / 5) + 32).toFixed(0)}°F`;
+
+const weatherData = async () => {
+  try {
+    const cityName = cityNameInput.value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myKey}`;
+    const response = await fetch(url, { mode: 'cors' });
+    const cityData = await response.json();
+    const city = await cityData;
+    cityTag.innerHTML = `${city.name} <span><sup>${city.sys.country}<sup></span>`;
+    cityTemp.innerHTML = kelvinToCelsius(city.main.temp);
+    date.innerHTML = `${Date().slice(16, 21)}, ${Date().slice(0, 10)}`;
+    cityNameInput.value = '';
+    wind.innerHTML = `Wind speed: ${city.wind.speed}m/s`;
+    visibility.innerHTML = `Visibility: ${(city.visibility) / 1000}km`;
+    humidity.innerHTML = `Humidity: ${city.main.humidity}%`;
+
+    tempCelcius.addEventListener('click', (e) => {
+      e.preventDefault();
+      cityTemp.innerHTML = kelvinToCelsius(city.main.temp);
+    });
+    tempFaren.addEventListener('click', (e) => {
+      e.preventDefault();
+      cityTemp.innerHTML = kelvinToFarenheit(city.main.temp);
+    });
+
+    if (city.main.temp >= 293) {
+      tempDescription.innerHTML = "It's a Sunny day";
+    } else {
+      tempDescription.innerHTML = "It's a Cold day";
+    }
+  } catch (err) {
+    cityNameInput.value = '';
+  }
+};
+
+getCity.addEventListener('click', (e) => {
+  e.preventDefault();
+  weatherData();
+});
